c367ecc0956493585bb445fa75cd90a50a70c001
danifer24 <danielvf24@gmail.com>
2025-04-07 16:25:16 +0200

    useState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 export const CounterApp = () => {
-    return <>
-    
-    </>
-};
+
+  const [counter, setCounter] = useState(0);
+
+  const counterIncrement = () => {
+    setCounter(counter + 1);
+    console.log("click!")
+  }
+
+  return <>
+    <h2>El valor del counter es: {counter}</h2>
+    <button onClick={() => counterIncrement()}  >incrementar contador + 1</button >
+  </>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { CounterApp } from './CounterApp.jsx'
 
-ReactDOM(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CounterApp />
   </React.StrictMode>,
